7e8d667a8ddf89df42aa3595c3319eefe2511097
Kaio <kaiocampus@outlook.com>
2024-10-08 08:52:09 -0300

    Subindo código atualizado

---
+++ src/components/HeaderHome.vue
@@ -0,0 +1,55 @@
+<script>
+export default{
+    name: "HeaderHome",
+}
+</script>
+<template>
+    <div>
+        <header id="header-home">
+            <nav>
+                <div class="span-links"><router-link to= "/" class="router-link"><span>Início</span></router-link></div>
+                <div class="span-links"><router-link to="/map" class="router-link"><span>Mapa</span></router-link></div>
+                <div class="span-links"><span>Sobre</span></div>
+               
+            </nav>
+        </header>
+    </div>
+</template>
+<style scoped>
+    @import '../assets/base.css';
+
+    header#header-home{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 70px;
+    }
+
+    nav{
+        display: flex;
+        width: 60%;
+        justify-content: space-between;
+        gap: 40px;
+        cursor: pointer;
+    }
+
+    div.span-links{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid black;
+        width: 100px;
+        height: 40px;
+        border-radius: 10px;
+    }
+
+    .router-link{
+        text-decoration: none;
+        color: black;
+    }
+    span{
+        font-family: 'Poppins';
+        font-weight: bolder;
+    }
+</style>
---
+++ src/components/HomeBody.vue
@@ -0,0 +1,51 @@
+<script>
+import HeaderHome from './HeaderHome.vue'
+export default{
+    name: "HomeBody",
+    components:{
+        HeaderHome
+    }
+}
+</script>
+
+<template>
+    <div id="bg-image">
+        <HeaderHome />
+        <div id="footer"><div class="text-footer"><h1>AirQuality</h1></div></div>
+    </div>
+</template>
+
+<style scoped>
+@import '../assets/base.css';
+
+    #bg-image{
+        background-image: url("../assets/images/foto-bolha-editada.jpg");
+        width: 100vw;
+        height: 100vh;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+    }
+
+
+    #footer{
+        width: 100%;
+        height: 15%;
+        position: absolute;
+        bottom: 0;
+    }
+
+    .text-footer{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 300px;
+    }
+
+    .text-footer > h1 {
+        font-family: 'Poppins';
+        color: #fff;
+        font-size: 60px;
+    }
+
+</style>
